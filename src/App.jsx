2f7fd5c492411f1b360819c6f3f68072555d9d83
liceth1006 <olmos.1006@gmail.com>
2024-04-23 21:42:22 -0500

    carousel

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+     <Navbar></Navbar>
+    </div>
+  );
+}
+
+export default App;
