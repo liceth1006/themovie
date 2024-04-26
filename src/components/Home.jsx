bb258772c33fa331b340662675c4a11cfcb00947
liceth1006 <olmos.1006@gmail.com>
2024-04-26 18:52:51 -0500

    .

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,25 +1,21 @@
-
-export default function Home ({ data, onClick }) {
-    return(
-        <div className="home">
-            {data && data.map((dataItem)=>{
-                return (
-                    <div
-                        key={dataItem.id}
-                        className="homeCard"
-                        // style={{
-                        //     background: `url(${dataItem.thumbnail.path}.${dataItem.thumbnail.extension}) no-repeat center`,
-                        //     backgroundSize:'cover'
-                        // }}
-                        onClick={()=> onClick(dataItem.id)}                    
-                        >
-
-                        <div className="caption">{dataItem.original_title}</div>
-                        <div className="caption bottom">view Comic</div>  
-                    </div>
-                );
-            })}
-
+ import React from 'react'
+ 
+ function Home({data, onclick}) {
+   return (
+    <div className="row row-cols-1 row-cols-md-3 g-4 m-4">
+    {data && data.map((dataItem) => (
+      <div className="col" key={dataItem.id}>
+        <div className="card h-100">
+          <img
+            src={`https://image.tmdb.org/t/p/w500${dataItem.poster_path}`}
+            className="card-img-top"
+            alt={dataItem.original_title}
+          />
         </div>
-    );
+      </div>
+    ))}
+  </div>
+);
 }
+ export default Home
+ 
