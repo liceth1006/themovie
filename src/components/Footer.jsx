680753847625f52f9ba0e40b16a2faefd66f5895
liceth1006 <olmos.1006@gmail.com>
2025-02-02 21:07:58 -0500

    .

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,6 @@ function Footer() {
   return (
 <div className="container">
   <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
-    <p className="col-md-4 mb-0 ">© 2024 Company, Inc</p>
 
     <div className="d-flex flex-column align-items-center">
             <span className="text-center text-color">
@@ -16,9 +15,6 @@ function Footer() {
             </span>
           </div>
     <div className=" col-md-4 justify-content-end">
-      <h3 className='text-color'>Santiago Parra</h3>
-      <h3 className='text-color'>Alex Rodriguez</h3>
-      <h3 className='text-color'> Liceth Olmos</h3>
     </div>
   </footer>
 </div>
